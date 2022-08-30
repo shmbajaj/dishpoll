b4d308ff15af872249da04f39759187f7719b062
Shubham Bajaj <shmbajaj32@gmail.com>
2022-08-31 01:47:11 +0530

    feat: source for root container and context provider

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { useUser, useDishes } from "context";
+import {
+  DishCardContainer,
+  Header,
+  Loader,
+} from "components";
+import { Login } from "container";
 
 function App() {
+  const { getUserInfo } = useUser();
+  const { getStatus } = useDishes();
+  const isUserLoggedIn = getUserInfo("isLoggedIn");
+  const status = getStatus();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="App">
+      {!isUserLoggedIn ? <Login /> : null}
+      {isUserLoggedIn ? <Header /> : null}
+      {isUserLoggedIn && status === "success" ? (
+        <DishCardContainer />
+      ) : null}
+      {isUserLoggedIn && status === "loading" ? (
+        <Loader />
+      ) : null}
+      {status === "failed" ? (
+        <h2 style={{ color: "red" }}>
+          Failed To Get Dishes.
+        </h2>
+      ) : null}
+    </main>
   );
 }
 
